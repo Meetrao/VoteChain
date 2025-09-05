5418fdb6c152397702b73ed43fc6d0e50191a8e0
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-09-05 16:06:24 +0530

    Voting Contract info in the frontend

---
+++ Frontend/my-app/src/contracts/ABI.js
@@ -0,0 +1,5 @@
+import CounterJson from "./Counter/Counter.json"
+import VotingJson from "./Voting/VotingModule#Voting.json"
+
+export const CounterABI = CounterJson.abi;
+export const VotingABI = VotingJson.abi;
--- Frontend/my-app/src/contracts/CouterABI.js
+++
@@ -1,3 +0,0 @@
-import CounterJson from "./Counter.json"
-
-export const CounterABI = CounterJson.abi;
